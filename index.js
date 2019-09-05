c027c8cc1fab70a429d1249d63b13c57e2a0a42b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-05 18:31:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   newDriver = driver;
   newDriver[key] = value;
-  return driver;
+  return newDriver;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
